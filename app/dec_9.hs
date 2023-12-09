c9fbbdafdcfab12cd6e510477d529fb518e37d4a
Clara Salberg <clarasalberg@live.com>
2023-12-09 14:54:18 +0100

    compacter

--- app/dec_9.hs
+++ app/dec_9.hs
@@ -1,14 +1,7 @@
 module Dec_9 where
-import Data.List.Extra (allSame)
-
-day9 :: IO ()
-day9 = do
-    ls <- map (map read . words) . lines <$> readFile "9th.txt"
-    print $ sum $ map (extrapolate (+) (\xs -> zipWith (-) xs (tail xs)) . reverse) ls
-    print $ sum $ map (extrapolate (-) (\xs -> zipWith (-) (tail xs) xs)) ls
-
-extrapolate :: (Int -> Int -> Int) -> ([Int] -> [Int]) -> [Int] -> Int
-extrapolate f g = foldr f 0 . go
-    where go ls
-            | allSame ls = [head ls]
-            | otherwise = head ls : go (g ls)
+main = do
+    l <- map (map read . words) . lines <$> readFile "9th"
+    print $ map sum [ (map (extr (+) last) l)
+                    , (map (extr (-) head) l)]
+  where extr f g = foldr f 0 . go where go l | all (== head l) l = [g l]
+                                             | True = g l : go (zipWith (-) (tail l) l)
